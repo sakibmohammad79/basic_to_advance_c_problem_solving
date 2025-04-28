e92458fb2b2eeeedc4c0fb036ec396b01341bab9
sakib <mohammadsakib7679@gmail.com>
2025-04-28 11:59:03 +0600

    solved salami and multiply problem

---
+++ Multiply.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    long long int num1, num2;
+    scanf("%lld %lld", &num1, &num2);
+    long long int mul = num1 * num2;
+    printf("%lld", mul);
+    return 0;
+}
---
+++ Salami.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int abul, babul, bablu;
+    scanf("%d %d %d", &abul, &babul, &bablu);
+    int total = abul + babul + bablu;
+    printf("%d", total);
+    return 0;
+}
