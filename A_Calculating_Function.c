379b752337756bd2abf947894dbd29f61e9d6154
sakib <mohammadsakib7679@gmail.com>
2025-04-30 15:18:16 +0600

    solved calculationg function problem

---
+++ A_Calculating_Function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    long long int n;
+    scanf("%lld", &n);
+    
+    if(n % 2 == 0)
+        printf("%lld\n", n / 2);
+    else
+        printf("%lld\n", -(n + 1) / 2);
+
+    return 0;
+}
