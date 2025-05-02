998e2174951ae9991a43c0da654b192d0a8d701c
sakib <mohammadsakib7679@gmail.com>
2025-05-02 18:17:11 +0600

    solve hulk emotion problem

---
+++ A_Hulk.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i<=n; i++){
+        if(i % 2 != 0){
+            printf("I hate ");
+        }
+        
+
+        if(i % 2 == 0){
+            printf("I love ");
+        }
+        if(i == n){
+            printf("it ");
+        }
+        else{
+            printf("that ");
+        }
+    }
+    return 0;
+}
